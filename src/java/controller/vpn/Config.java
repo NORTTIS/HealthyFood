fa337a07359bb90e2109b9c5e1c946b1cc36f35f
Minh <Minh@Asus>
2024-10-03 15:41:17 +0700

    test sonarqube2

--- src/java/controller/vpn/Config.java
+++ src/java/controller/vpn/Config.java
@@ -14,6 +14,7 @@ import java.util.Random;
 import javax.crypto.Mac;
 import javax.crypto.spec.SecretKeySpec;
 import jakarta.servlet.http.HttpServletRequest;
+import java.security.SecureRandom;
 
 /**
  *
@@ -120,8 +121,8 @@ public class Config {
         return ipAdress;
     }
 
-    public static String getRandomNumber(int len) {
-        Random rnd = new Random();
+    public static String getRandomNumber(int len) throws NoSuchAlgorithmException {
+        Random rnd = SecureRandom.getInstanceStrong();
         String chars = "0123456789";
         StringBuilder sb = new StringBuilder(len);
         for (int i = 0; i < len; i++) {
--- web/vnpay_return.jsp
+++ web/vnpay_return.jsp
@@ -3,13 +3,6 @@
 
 <%@page contentType="text/html" pageEncoding="UTF-8"%>
 
-<%@page import="java.util.Iterator"%>
-<%@page import="java.util.Collections"%>
-<%@page import="java.util.List"%>
-<%@page import="java.util.ArrayList"%>
-<%@page import="java.util.Enumeration"%>
-<%@page import="java.util.Map"%>
-<%@page import="java.util.HashMap"%>
 
 <!DOCTYPE html>
 <html lang="en">
