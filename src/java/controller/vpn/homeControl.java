8ef35bd991453b1db25f33ee8ea2626f6add8578
Minh <Minh@Asus>
2024-10-03 14:44:13 +0700

    test sonarqube

--- src/java/controller/vpn/homeControl.java
+++ src/java/controller/vpn/homeControl.java
@@ -2,16 +2,13 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
  */
-
 package controller.vpn;
 
 import dao.DBContext;
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import jakarta.servlet.ServletException;
-import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -22,31 +19,33 @@ import java.sql.Connection;
  * @author Norttie
  */
 public class homeControl extends HttpServlet {
-   
-    /** 
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
+            throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         try {
-             Connection conn = new DBContext().getConnection();
-             String sql = "select count(*) from Surveys";
-             PreparedStatement questionStmt = conn.prepareStatement(sql);
+            Connection conn = new DBContext().getConnection();
+            String sql = "select count(*) from Surveys";
+            PreparedStatement questionStmt = conn.prepareStatement(sql);
             ResultSet rsCount = questionStmt.executeQuery();
-             int countSurvey = 0 ;
-             int countUserCount = 0 ;
-            if(rsCount.next()){
+            int countSurvey = 0;
+            int countUserCount = 0;
+            if (rsCount.next()) {
                 countSurvey = rsCount.getInt(1);
             }
             String sql2 = "select count(*) from Users";
             PreparedStatement userNumber = conn.prepareStatement(sql2);
             ResultSet rsUsercount = userNumber.executeQuery();
-            if(rsUsercount.next()){
+            if (rsUsercount.next()) {
                 countUserCount = rsUsercount.getInt(1);
             }
             request.setAttribute("snum", countSurvey);
@@ -54,11 +53,12 @@ public class homeControl extends HttpServlet {
             request.getRequestDispatcher("index.jsp").forward(request, response);
         } catch (Exception e) {
         }
-    } 
+    }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /** 
+    /**
      * Handles the HTTP <code>GET</code> method.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
@@ -66,12 +66,13 @@ public class homeControl extends HttpServlet {
      */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
+            throws ServletException, IOException {
         processRequest(request, response);
-    } 
+    }
 
-    /** 
+    /**
      * Handles the HTTP <code>POST</code> method.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
@@ -79,12 +80,13 @@ public class homeControl extends HttpServlet {
      */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
+            throws ServletException, IOException {
         processRequest(request, response);
     }
 
-    /** 
+    /**
      * Returns a short description of the servlet.
+     *
      * @return a String containing servlet description
      */
     @Override
--- web/vnpay_return.jsp
+++ web/vnpay_return.jsp
@@ -1,16 +1,4 @@
-<%@page import="java.net.URLEncoder"%>
-<%@page import="java.nio.charset.StandardCharsets"%>
-
 <%@page contentType="text/html" pageEncoding="UTF-8"%>
-
-<%@page import="java.util.Iterator"%>
-<%@page import="java.util.Collections"%>
-<%@page import="java.util.List"%>
-<%@page import="java.util.ArrayList"%>
-<%@page import="java.util.Enumeration"%>
-<%@page import="java.util.Map"%>
-<%@page import="java.util.HashMap"%>
-
 <!DOCTYPE html>
 <html lang="en">
     <head>
