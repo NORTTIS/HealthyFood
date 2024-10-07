cce3f92e0245555ef98ce6d5163c0b3853eadc56
Minh <Minh@Asus>
2024-10-07 15:04:06 +0700

    vnpay change

--- src/java/controller/vpn/Config.java
+++ src/java/controller/vpn/Config.java
@@ -14,7 +14,6 @@ import java.util.Random;
 import javax.crypto.Mac;
 import javax.crypto.spec.SecretKeySpec;
 import jakarta.servlet.http.HttpServletRequest;
-import java.security.SecureRandom;
 
 /**
  *
@@ -120,8 +119,8 @@ public class Config {
         return ipAdress;
     }
 
-    public static String getRandomNumber(int len) throws NoSuchAlgorithmException {
-        Random rnd = SecureRandom.getInstanceStrong();
+    public static String getRandomNumber(int len) {
+        Random rnd = new Random();
         String chars = "0123456789";
         StringBuilder sb = new StringBuilder(len);
         for (int i = 0; i < len; i++) {
