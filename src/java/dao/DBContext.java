cfcb864225a218e59c66abc422a2e64474de2d58
Gosu <Gosu@DESKTOP-65G7P6E>
2024-10-19 23:23:19 +0700

    đạt bíeet git

--- src/java/dao/DBContext.java
+++ src/java/dao/DBContext.java
@@ -21,7 +21,7 @@ public class DBContext {
 
             String url = "jdbc:sqlserver://localhost:1433;databaseName=HealthyFood";
             String username = "sa";
-            String password = "123456";
+            String password = "123";
             Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
             connection = DriverManager.getConnection(url, username, password);
         } catch (ClassNotFoundException | SQLException ex) {
